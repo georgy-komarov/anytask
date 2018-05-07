4c62d1bc60bde770aeecdca6a0c6c2a6e246d075
Georgy <georgy.komarov@mail.ru>
2018-05-07 17:15:40 +0300

    Change e-mail

--- anytask/generate_test_db.sh
+++ anytask/generate_test_db.sh
@@ -4,5 +4,5 @@
 
 ./manage.py syncdb --noinput
 ./manage.py migrate
-./manage.py createsuperuser --username=anytask --email=anytask@yandex.ru --noinput
+./manage.py createsuperuser --username=anytask --email=anytask-kaluga@mail.ru --noinput
 echo 'from django.contrib.auth.models import User ; user=User.objects.get(username="anytask") ; user.set_password("pass") ; user.save() ; print "Password changed"' | ./manage.py shell --plain
